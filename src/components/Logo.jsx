a058ab4627a5ded2e9c0ee13faf0fea4d595827d
DeepakIITP <deepak_2312res235@iitp.ac.in>
2025-08-06 20:27:02 +0530

    Make Logo and Menu

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+function Logo() {
+  return <img src={assets.logo} alt="logo" height={48} width={48} />;
+}
+
+export default Logo;    
