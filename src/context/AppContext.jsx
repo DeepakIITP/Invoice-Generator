8279cc30b229b49301371db77c77303e983965a6
DeepakIITP <deepak_2312res235@iitp.ac.in>
2025-08-07 10:36:12 +0530

    Created App contexts

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+const initialInvoiceData = {
+    title: "New Invoice",
+    billing: { name: "", phone: "", address: "" },
+    shipping: { name: "", phone: "", address: "" },
+    invoice: { number: "", date: "", dueDate: "" },
+    account: { name: "", number: "", ifscode: "" },
+    company: { name: "", number: "", address: "" },
+    tax: 0,
+    notes: "",
+    items: [{ name: "", qty: "", amount: "", description: "", total: 0 }],
+    logo: "",
+};
+const baseUrl = "http://localhost:3510/api";
+export const AppContextProvider = ({ children }) => {
+    const [invoiceTitle, setInvoiceTitle] = useState("New Invoice");
+    const [invoiceData, setInvoiceData] = useState(initialInvoiceData);
+    const [selectedTemplate, setSelectedTemplate] = useState();
+    const contextValue = {
+        invoiceTitle,
+        setInvoiceTitle,
+        invoiceData,
+        setInvoiceData,
+        selectedTemplate,
+        setSelectedTemplate,
+        baseUrl,
+        initialInvoiceData,
+    };
+    return (
+        <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+    );
+};
