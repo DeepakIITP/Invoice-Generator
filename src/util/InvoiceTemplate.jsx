871eb8d357e8d61f96492cd9fcf86e564668b615
DeepakIITP <deepak_2312res235@iitp.ac.in>
2025-08-07 12:31:49 +0530

    Design all the templates

---
+++ src/util/InvoiceTemplate.jsx
@@ -0,0 +1,13 @@
+import Template1 from "../template/template1/Template1";
+import Template2 from "../template/template2/Template2";
+import Template3 from "../template/template3/Template3";
+import Template4 from "../template/template4/Template4";
+import Template5 from "../template/template5/Template5";
+
+export const templateComponents = {
+  template1: Template1,
+  template2: Template2,
+  template3: Template3,
+  template4: Template4,
+  template5: Template5,
+};
